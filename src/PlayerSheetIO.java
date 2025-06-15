5bdeca552b09ccb178cb9f1cd8f3ce42ffba66f5
SkylerWolf127 <156957978+SkylerWolf127@users.noreply.github.com>
2025-06-15 15:17:41 -0600

    Bakcup

--- src/PlayerSheetIO.java
+++ src/PlayerSheetIO.java
@@ -1,7 +1,8 @@
 import java.io.*;
 public class PlayerSheetIO
 {
-    public static void savePlayerSheetToFile(PlayerSheet sheet, String filename) {
+    public static void savePlayerSheetToFile(PlayerSheet sheet, String filename)
+    {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
             oos.writeObject(sheet);
             System.out.println("Player sheet saved to " + filename);
@@ -10,7 +11,8 @@ public class PlayerSheetIO
         }
     }
 
-    public static PlayerSheet loadPlayerSheetFromFile(String filename) {
+    public static PlayerSheet loadPlayerSheetFromFile(String filename)
+    {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
             PlayerSheet sheet = (PlayerSheet) ois.readObject();
             System.out.println("Player sheet loaded from " + filename);
--- src/playerSpecies.java
+++ src/playerSpecies.java
@@ -1,5 +1,7 @@
 public class PlayerSpecies extends PlayerSheet
 {
+
+    //TODO: Dictionary for potential player races. Take information from 2024 Player's Handbook.
     private String speciesName;
 
 
